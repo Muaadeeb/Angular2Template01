b8ed145497ad46487b054f1a630402053b73cdef
Timothy Ingledue <Tim.Ingledue@fcsamerica.com>
2016-10-06 14:59:36 -0500

    #0001 - Change Main Page Text.

--- Angular2Template01/app/app.component.js
+++ Angular2Template01/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My Second Angular App</h1>'
+            template: '<h1>Welcome to Angular 2 - Visual Studio 2015</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
