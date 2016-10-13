ce489cb58353128ebf4c4221a9acf7753caac27a
Timothy Ingledue <Tim.Ingledue@fcsamerica.com>
2016-10-13 17:06:47 -0500

    #0000 - WIP

--- Angular2Template01/app/app.component.js
+++ Angular2Template01/app/app.component.js
@@ -11,11 +11,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Ultra Racing';
+        this.carParts = [{
+                "id": 1,
+                "name": "Super Tires",
+                "description": "These tires are the very best",
+                "inStock": 5,
+                "price": 4.99,
+                "today": new Date('2016-10-01T12:00:00')
+            },
+            {
+                "id": 2,
+                "name": "Reinforced Shocks",
+                "description": "Shocks made from kryptonite",
+                "inStock": 4,
+                "price": 9.99,
+                "today": new Date('2016-10-05T12:00:00')
+            },
+            {
+                "id": 3,
+                "name": "Padded Seats",
+                "description": "Super soft seats for a smooth ride",
+                "inStock": 0,
+                "price": 24.99,
+                "today": new Date('2016-10-10T12:00:00')
+            }];
+        this.showDate = Date.now();
     }
+    AppComponent.prototype.totalCarParts = function () {
+        var sum = 0;
+        for (var _i = 0, _a = this.carParts; _i < _a.length; _i++) {
+            var carPart = _a[_i];
+            sum += carPart.inStock;
+        }
+        return sum;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Welcome to Angular 2 - Visual Studio 2015</h1>'
+            template: "<h1>{{title}}</h1>\n        <p>There are {{totalCarParts()}} total parts in stock.</p>\n        <p> {{showDate | date: 'medium'}} </p>\n        <ul>\n            <li *ngFor=\"let carPart of carParts\">\n                <h2>{{carPart.name | uppercase}}</h2>\n                <p>{{carPart.description}}</p>\n                <p>{{carPart.price | currency: 'EUR':true}}</p>\n                <p *ngIf=\"carPart.inStock > 0\">{{carPart.inStock}} in Stock</p>\n                <p *ngIf=\"carPart.inStock === 0\">Out of Stock</p>\n                <p>Date available: {{carPart.today | date: 'medium'}}</p>\n            </li>\n        </ul>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
